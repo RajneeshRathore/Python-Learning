ef88148bfc372b7b1a10d3c1ab9f2d8daa6816e8
Rajneesh Rathore <rajneeshrathore63@gmail.com>
2025-11-15 15:25:14 +0530

    initial commit

--- variable.py
+++ variable.py
@@ -1,5 +1,5 @@
 # variables are the containers which are used to store the data.
 
-user_name="Rajneesh Rathore"
-user_age=20
-print(f'User name is {user_name} and  user age equal to {user_age} ')
+username="Rajneesh Rathore"
+userage=20
+print(f'User name is {username} and  user age equal to {userage} ')
