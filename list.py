6962bb4f094367f5c241a090b132d4901e7b3df8
Rajneesh Rathore <rajneeshrathore63@gmail.com>
2026-01-17 13:15:16 +0530

    list

---
+++ list.py
@@ -0,0 +1,8 @@
+# list is an build in data type in Python that represents a collection of items.
+# Lists are ordered, mutable (changeable), and allow duplicate values.
+
+my_list = list(range(1,21))
+print("Original list:", my_list)
+
+my_list.append(21)
+print("After appending 21:", my_list)
